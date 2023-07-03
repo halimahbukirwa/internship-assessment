7434c0ca790be1fa0b73c98c99de4ac441faf5cf
Halimah Bukirwa <halimahbukirwa24@gmail.com>
2023-07-03 16:28:28 +0300

    Adding comments

--- translate.py
+++ translate.py
@@ -1,5 +1,6 @@
 import requests
 
+#calling the translation endpoint
 url = 'https://sunbird-ai-api-5bq6okiwgq-ew.a.run.app'
 
 headers = {
@@ -10,18 +11,26 @@ print('\n')
 print('TEXT TO-AND-FROM ENGLISH TO 5 LOCAL UGANDAN LANGUAGES TRANSLATOR')
 print('\n')
 
+#The user enters the source language
 source_language = input('Please choose the source language:')
 
+#The user enters the target language
 target_language = input('Please choose the target language: (one of Luganda, Runyankole, Ateso, Lugbara or Acholi):')
 
+#The user enters the text to be translated from the source language to the target language
 text = input('Enter the text to translate:')
+
+#The text should having 3 or more characters, an error occurs
 if len(text) >= 3:
     payload = {
     "source_language": source_language,
     "target_language": target_language,
     "text": text }
+
+    #Passing data obtained from the users into the /translate endpoint
     response = requests.post(f"{url}/tasks/translate", headers=headers, json=payload)
 
+    #Response generation
     if response.status_code == 200:
         translated_text = response.json()["text"]
         print("Translate text:", translated_text)
